4968e74cf39e4d07cbf6aab8b8ca0a14ff0e2489
Yash kumar siddharth0 <ykumarsiddharth@gmail.com>
2025-07-18 23:57:03 +0530

    Updated backend CORS to allow Vercel frontend

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,11 @@ const server = http.createServer(app)
 
 const io = new Server(server, { 
     cors: {
-        origin: "http://localhost:3000",
+        origin: [
+            "http://localhost:3000",
+            "https://realtimechatapp-seven.vercel.app"
+        ],
+        
         methods: ["GET", "POST"]
     }
 })
